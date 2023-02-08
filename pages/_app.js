a0ba1111f21eb30aeb2330274e56fc72d7c8f439
Alexa spalato <alexaspalato@gmail.com>
2023-02-08 14:31:10 +0100

    basic implementation

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
-import '~/styles/globals.css'
+import "~/styles/globals.css";
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+import Grid from "~/components/Grid";
+import Page from "~/components/Page";
+import Teaser from "~/components/Teaser";
+import Feature from "~/components/Feature";
+
+const components = {
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  page: Page,
+};
+
+storyblokInit({
+  accessToken: "57h44lHQla035157swwjLQtt",
+  use: [apiPlugin],
+  components,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
